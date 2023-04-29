576d6e4a6b758d1ced1eab1842979c80d9f3ac5f
guofeihong_he <1826252461@qq.com>
2023-04-29 17:53:36 +0800

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# ChatRot(纯前端版)
+
+#### 介绍
+
+基于react的chatgpt聊天机器人，页面使用了蚂蚁的chatUI组件库，简单易上手，让你十分钟搭建属于自己的chatgpt问答机器人！
+
+#### 使用说明
+
+1.  本项目使用GPT-3.5-turbo,支持记录上下文实现连续对话！
+
+2.  首次使用需要设置自己的 openai 的api key,api key保存本地,不会有安全问题！
+
+3.  项目纯前端页面,使用gitee pages或者 github pages部署就行！
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm install`
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
---
+++ src/App.js
@@ -0,0 +1,232 @@
+import './App.css';
+
+import '@chatui/core/es/styles/index.less';
+// 引入组件
+import Chat, { Bubble, useMessages, Button, Modal, Input, Divider, toast, RichText } from '@chatui/core';
+// 引入样式
+import '@chatui/core/dist/index.css';
+import axios from "axios";
+
+import { useState } from 'react';
+const initialMessages = [
+  {
+    type: 'text',
+    content: { text: '主人好，我是基于chatGPT的chatBot，可以回答问题、翻译、写作等，欢迎随时与我交流~' },
+    user: { avatar: '//gw.alicdn.com/tfs/TB1DYHLwMHqK1RjSZFEXXcGMXXa-56-62.svg' },
+  },
+  // {
+  //   type: 'image',
+  //   content: {
+  //     picUrl: '//img.alicdn.com/tfs/TB1p_nirYr1gK0jSZR0XXbP8XXa-300-300.png',
+  //   },
+  // },
+];
+
+// 默认快捷短语，可选
+const defaultQuickReplies = [
+  {
+    name: '自我介绍一下',
+    isNew: true,
+  },
+  {
+    name: '讲个笑话',
+    // isHighlight: true,
+  },
+  {
+    icon: "cancel",
+    name: '刷新页面',
+    // isHighlight: true,
+  },
+  {
+    icon: 'plus-circle',
+    name: 'openAI设置',
+    // isNew: true,
+    isHighlight: false,
+  },
+  {
+    icon: "folder",
+    name: '前端笔记',
+    // isHighlight: true,
+  },
+];
+
+export default function () {
+  // 消息列表
+  const { messages, appendMsg, setTyping } = useMessages(initialMessages);
+  // 输入Key
+  const [open, setOpen] = useState(false);
+  const [value1, setValue1] = useState('');
+  function handleOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
+  function handleConfirm() {
+    // alert(value1);
+    if (value1.length < 20) {
+      toast.fail("格式不正确")
+      return
+    }
+    localStorage.setItem('apiKey', value1)
+    toast.success("操作成功")
+    setOpen(false);
+  }
+
+  // 发送回调
+  // function handleSend(type, val) {
+  //   if (type === 'text' && val.trim()) {
+  //     // TODO: 发送请求
+  //     appendMsg({
+  //       type: 'text',
+  //       content: { text: val },
+  //       position: 'right',
+  //     });
+
+  //     setTyping(true);
+
+  //     // 模拟回复消息
+  //     setTimeout(() => {
+  //       appendMsg({
+  //         type: 'text',
+  //         content: { text: '亲，您遇到什么问题啦？请简要描述您的问题~' },
+  //       });
+  //     }, 1000);
+  //   }
+  // }
+
+  const [chatMessage, setChatMessage] = useState([]);
+  // 发送回调
+  function handleSend(type, val) {
+    var apiKey = localStorage.getItem('apiKey')
+    if (!apiKey) {
+      // toast.fail("请先输入你的apiKey")
+      setOpen(true);
+      return
+    }
+    if (type === 'text' && val.trim()) {
+      // TODO: 发送请求
+      appendMsg({
+        type: 'text',
+        content: { text: val },
+        position: 'right',
+      });
+      axios
+        .post(
+          // 'https://api.openai.com/v1/chat/completions',
+          'https://open.aiproxy.xyz/v1/chat/completions',
+          {
+            messages: [...chatMessage, { content: val, role: 'user' }],
+            max_tokens: 2048,
+            n: 1,
+            temperature: 0.5,
+            //   stop: ['\n'],
+            model: 'gpt-3.5-turbo',
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: 'Bearer ' + apiKey,
+            },
+          },
+        )
+        .then((res) => {
+          const response = res.data.choices[0].message.content.trim();
+          const newMessages = [
+            ...chatMessage,
+            { content: val, role: 'user' },
+            { content: response, role: 'system' },
+          ];
+          // setChatMessage(newMessages as any);
+          setChatMessage(newMessages);
+          appendMsg({
+            type: 'text',
+            content: { text: response },
+            user: { avatar: './ChatGPT_logo.png' },
+          });
+        })
+        .catch((err) => {
+          console.log(err)
+          toast.fail("出错啦！请稍后再试")
+        }
+        );
+
+      setTyping(true);
+    }
+  }
+
+  // 快捷短语回调，可根据 item 数据做出不同的操作，这里以发送文本消息为例
+  function handleQuickReplyClick(item) {
+    console.log(item.name);
+    if (item.name == "openAI设置") {
+      handleOpen()
+      return
+    }
+    if (item.name == "自我介绍一下") {
+      defaultQuickReplies[0].isNew = false
+    }
+    if (item.name == "刷新页面") {
+      window.location.reload()
+      return
+    }
+    if (item.name == "前端笔记") {
+      window.location = "https://g_dev.gitee.io/vue-press/#/"
+      return
+    }
+    handleSend('text', item.name);
+  }
+
+  function renderMessageContent(msg) {
+    const { type, content } = msg;
+
+    // 根据消息类型来渲染
+    switch (type) {
+      case 'text':
+        return <Bubble content={content.text} />;
+      case 'image':
+        return (
+          <Bubble type="image">
+            <img src={content.picUrl} alt="" />
+          </Bubble>
+        );
+      default:
+        return null;
+    }
+  }
+
+  return (
+    [<Chat
+      navbar={{ title: 'chatBot' }}
+      placeholder="有问题尽管问我~"
+      messages={messages}
+      renderMessageContent={renderMessageContent}
+      quickReplies={defaultQuickReplies}
+      onQuickReplyClick={handleQuickReplyClick}
+      onSend={handleSend}
+    />,
+    <Modal
+      active={open}
+      title="设置"
+      showClose={false}
+      onClose={handleClose}
+      actions={[
+        {
+          label: '确认',
+          color: 'primary',
+          onClick: handleConfirm,
+        },
+        {
+          label: '取消',
+          onClick: handleClose,
+        },
+      ]}
+    >
+      <div>
+        <p><span className='requird-span'>*</span>API Key</p>
+        <Input value={value1} onChange={val => setValue1(val)} placeholder="请输入API Key" /></div>
+    </Modal>
+    ]
+  );
+}
